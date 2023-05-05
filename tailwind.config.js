59606878680923d058e507fd8c2dafe9c30515ac
Hadassah <faridathadassah4@gmail.com>
2023-05-05 15:00:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Moderateviolet: '#7541c8',
+        Verydarkgrayishblue: '#48556a',
+        Verydarkblackishblue: '#19212e',
+        White: '#ffffff',
+        Lightgray: ' #cfcfcf',
+        Lightgrayishblue: '#ecf2f8'
+       
+       
+      },
+      fontFamily: {
+        BarlowSemiCondensed: ['Barlow Semi Condensed', 'sans-serif' ] 
+      }
+    }
+  },
+  plugins: [],
+}
+ 
